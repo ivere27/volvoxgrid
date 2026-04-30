195f1ce86dfd56acf18b023bd258a1443cc124fa
yONg <ivere27@gmail.com>
2026-04-30 21:55:45 +0900

    Package.swift v0.8.3

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .binaryTarget(
             name: "VolvoxGridPlugin",
             url: "https://github.com/ivere27/volvoxgrid/releases/download/v0.8.3/VolvoxGridPlugin.xcframework.zip",
-            checksum: "389ea666e94b54e1950fa9249ccd1aa19c64a490ddf4d3185d7d18e0d499a7fa"
+            checksum: "92a9b54d32e81a8c0c5bf3c5e6b56a3af460ded6ac2413a94dc601fa1a807802"
         ),
     ]
 )
